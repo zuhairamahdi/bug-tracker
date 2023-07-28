6ce432cc5646da77243bd59012bdf258126291a9
Zuhair Abdulla <zuhairamahdi@gmail.com>
2023-07-28 14:21:50 +0300

    Add IsColorHex to extentions

--- ext/extentions.go
+++ ext/extentions.go
@@ -1,8 +1,12 @@
 package ext
 
-import "math/rand"
+import (
+	"math/rand"
+	"regexp"
+)
 
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const colorRegex = `/^#([0-9A-F]{3}){1,2}$/i`
 
 func RandStringBytes(n int) string {
 	b := make([]byte, n)
@@ -10,4 +14,11 @@ func RandStringBytes(n int) string {
 		b[i] = letterBytes[rand.Intn(len(letterBytes))]
 	}
 	return string(b)
+}
+func IsColorHex(n string) bool {
+	matched, _ := regexp.MatchString(colorRegex, n)
+	if matched {
+		return matched
+	}
+	return false
 }
