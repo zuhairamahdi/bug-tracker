c32212efcb709915e71e5ad106fa6db19df5ed4d
Zuhair Abdulla <zuhairamahdi@gmail.com>
2023-07-28 12:56:28 +0300

    Add Color col to Column model

--- models/Column.go
+++ models/Column.go
@@ -10,6 +10,7 @@ type Column struct {
 	ID         string `json:"id" gorm:"primaryKey;autoIncrement:false"`
 	Title      string `json:"title" gorm:"index"`
 	Desciption string `json:"description"`
+	Color      string `json:"color"`
 	CreatedAt  time.Time
 	UpdatedAt  time.Time
 	DeletedAt  gorm.DeletedAt `gorm:"index"`
