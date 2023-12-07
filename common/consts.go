0d7b84ca817df4d6beea80486bca34b6800e4412
Zuhair Almughlaq <zuhairamahdi@gmail.com>
2023-12-07 12:00:17 +0300

    Add Task statuses consts to common.consts

--- common/consts.go
+++ common/consts.go
@@ -7,3 +7,14 @@ const (
 	ProjectOwnerRole = "project_owner"
 	ViewerRole       = "viewer"
 )
+
+// Task statuses
+const (
+	OpenStatus       = "open"
+	InProgressStatus = "in_progress"
+	RemovedStatus    = "removed"
+	RejectedStatus   = "rejected"
+	DoneStatus       = "done"
+	CanceledStatus   = "canceled"
+	AcceptedStatus   = "accepted"
+)
--- ext/extentions.go
+++ ext/extentions.go
@@ -8,6 +8,21 @@ import (
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const colorRegex = `/^#([0-9A-F]{3}){1,2}$/i`
 
+// email regex
+var emailRegex = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
+func RandomString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+func IsValidEmail(email string) bool {
+	return emailRegex.MatchString(email)
+}
+
 func RandStringBytes(n int) string {
 	b := make([]byte, n)
 	for i := range b {
